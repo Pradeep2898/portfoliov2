4077e6734aad7c798038a19c2147f904d742fca6
Pradeep Anand <spanand2898@gmail.com>
2021-10-01 01:07:48 +0530

    added png image to meta image

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "../styles/output.css";
 import "./about.css";
 import { GrLinkedin, GrGithub } from "react-icons/gr";
